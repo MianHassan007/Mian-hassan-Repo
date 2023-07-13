37d29bda7559b55bf1ed87eb3b172a0e2960070d
MianHassan007 <132878100+MianHassan007@users.noreply.github.com>
2023-07-13 23:53:58 +0500

    Khaadi Portfolio File

---
+++ Navbar hover with JS function/Khaadi Portfolio/JS/Khaadi Portfolio resp.js
@@ -0,0 +1,26 @@
+
+// ------------------------------------------SIDEBAR----------------------------------------------------
+
+/* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
+function openNav() {
+    document.getElementById("mySidebar").style.width = "450px";
+    document.getElementById("main").style.marginLeft = "250px";
+  }
+  
+  /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+  function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+  }
+
+  // ------------------------------------------Collapsible dropdown menu----------------------------------------------------
+
+  function showContent(index) {
+    const content = document.getElementsByClassName('subnav-content')[index];
+    content.style.display = 'block';
+  }
+  
+  function hideContent(index) {
+    const content = document.getElementsByClassName('subnav-content')[index];
+    content.style.display = 'none';
+  }
